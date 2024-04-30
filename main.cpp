905d8cb30a5338127d3261e6263d4f8618d52d87
James Fogle <jfogle53@gmail.com>
2024-04-29 22:03:46 -0400

    First creation of application. Small example code.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    cout << "Give this another try" << endl;
+    // my comment for the first time.
+}
